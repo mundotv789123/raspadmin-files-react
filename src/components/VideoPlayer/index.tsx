96b225623743ae386e0e6b2f190cc1e8f4bc829b
mundotv789123 <789michaelfernandes789@gmail.com>
2023-09-02 16:59:41 -0300

    Refatoração VideoPlayer

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,58 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faVolumeUp, faExpand, faAngleLeft, faPause } from '@fortawesome/free-solid-svg-icons'
 import { useRef, useState } from "react";
-import md5 from "md5";
 import { VideoBottom, VideoButton, VideoCenter, VideoCloseButton, VideoCont, VideoElement, VideoLoading, VideoMain, VideoProgress, VideoProgressBar, VideoProgressFollower, VideoTitle, VideoTop, VideoVolume } from './styles';
+import VideoService from '../../services/VideoService';
 
-export function getVideoTime(url) {
-    if (!localStorage['videos']) {
-        localStorage['videos'] = JSON.stringify({});
-        return 0;
-    }
-    let videos = JSON.parse(localStorage['videos']);
-    let uuid = md5(url);
-    return videos[uuid] ? videos[uuid] : 0;
-}
-
-export function setVideoTime(url, time) {
-    if (time <= 0) {
-        return;
-    }
-    let videos = localStorage['videos'] ? JSON.parse(localStorage['videos']) : {};
-    let uuid = md5(url);
-    videos[uuid] = parseFloat(time);
-    localStorage['videos'] = JSON.stringify(videos);
-}
-
-export default function VideoPlayer(props) {
-    /* states */
-    const [buttonPlayIcon, setButtonPlayIcon] = useState(faPlay);
+export default function VideoPlayer(props: { src: string | undefined, backUrl: string | undefined }) {
     const [progressPercent, setProgressPercent] = useState(0);
     const [progressFollowerPercent, setProgressFollerPercent] = useState(0);
-    const [loading, setLoading] = useState(true)
 
-    const main_element = useRef(null);
-    const video_element = useRef(null);
-    const progress_follower = useRef(null);
-    const progress_bar = useRef(null);
-    const volume = useRef(null);
-    const volume_percent = useRef(null);
+    const [loading, setLoading] = useState(true);
+    const [playing, setPlaying] = useState(false);
+
+    const main_element = useRef<HTMLDivElement>(null);
+    const progress_follower = useRef<HTMLDivElement>(null);
+    const progress_bar = useRef<HTMLDivElement>(null);
+    const volume = useRef<HTMLDivElement>(null);
+    const volume_percent = useRef<HTMLDivElement>(null);
+    const video_element = useRef<HTMLVideoElement>(null);
+
+    const service = new VideoService();
 
     function togglePauseVideo() {
-        if (video_element.current.paused) {
-            video_element.current.play();
-        } else {
+        if (playing) {
             video_element.current.pause();
+        } else {
+            video_element.current.play();
         }
         setLoading(false);
     }
 
     function togglePauseButton() {
-        if (video_element.current.paused) {
-            setButtonPlayIcon(faPlay)
-        } else {
-            setButtonPlayIcon(faPause)
-        }
+        setPlaying(!video_element.current.paused);
     }
 
     function toggleFullScreen() {
@@ -68,20 +46,20 @@ export default function VideoPlayer(props) {
         setProgressPercent(percent);
     }
 
-    function updateProgressFollower(event) {
+    function updateProgressFollower(event: any) {
         let rect = progress_bar.current.getBoundingClientRect();
         let percent = ((event.clientX - rect.left) * 100 / (rect.right - rect.left));
         setProgressFollerPercent(percent);
     }
 
-    function updateVideoTime(event) {
+    function updateVideoTime(event: any) {
         let rect = progress_bar.current.getBoundingClientRect();
         let percent = ((event.clientX - rect.left) * 100 / (rect.right - rect.left));
         video_element.current.currentTime = (video_element.current.duration / 100 * percent);
         setProgressPercent(percent);
     }
 
-    function updateVolume(event) {
+    function updateVolume(event: any) {
         let rect = volume.current.getBoundingClientRect();
         let value = ((rect.bottom - event.clientY) * 1.0 / (rect.bottom - rect.top));
         video_element.current.volume = value;
@@ -90,7 +68,7 @@ export default function VideoPlayer(props) {
 
     function resetVideo() {
         video_element.current.src = null;
-        setButtonPlayIcon(faPlay);
+        setPlaying(false);
         setProgressPercent(0);
         setLoading(true);
     }
@@ -100,24 +78,12 @@ export default function VideoPlayer(props) {
             return;
         }
         volume_percent.current.style.height = (video_element.current.volume * 100) + '%';
-        let videoTime = getVideoTime(video_element.current.src);
+        let videoTime = service.getVideoTime(video_element.current.src);
         if (videoTime > 0 && (video_element.current.duration - 15) > videoTime) {
             video_element.current.currentTime = videoTime;
         }
         setLoading(false);
-        updateTime(true);
-    }
-
-    async function updateTime(loop = false) {
-        if (!video_element.current || !video_element.current.src) {
-            return;
-        }
-        setVideoTime(video_element.current.src, video_element.current.currentTime);
-        if (loop) {
-            setTimeout(() => {
-                updateTime(true)
-            }, 1000);
-        }
+        service.startAutoSaving(video_element.current.src, video_element.current);
     }
 
     if (props.src == null) {
@@ -136,7 +102,13 @@ export default function VideoPlayer(props) {
             <VideoMain>
                 <VideoTop>
                     <VideoTitle>{fileName.substring(0, 32) + (fileName.length > 32 ? '...' : '')}</VideoTitle>
-                    <VideoCloseButton style={{ display: (props.backUrl ? '' : 'none') }} href={props.backUrl} onClick={resetVideo}>
+                    <VideoCloseButton
+                        style={{
+                            display: (props.backUrl ? '' : 'none')
+                        }}
+                        href={props.backUrl}
+                        onClick={resetVideo}
+                    >
                         <FontAwesomeIcon icon={faAngleLeft} />
                     </VideoCloseButton>
                 </VideoTop>
@@ -145,16 +117,26 @@ export default function VideoPlayer(props) {
                 </VideoCenter>
                 <VideoBottom>
                     <VideoButton onClick={togglePauseVideo}>
-                        <FontAwesomeIcon icon={buttonPlayIcon} />
+                        <FontAwesomeIcon icon={(playing ? faPause : faPlay)} />
                     </VideoButton>
-                    <VideoProgress onClick={(e) => updateVideoTime(e)} onMouseMove={(e) => updateProgressFollower(e)} ref={progress_bar}>
+                    <VideoProgress
+                        onClick={updateVideoTime}
+                        onMouseMove={updateProgressFollower}
+                        ref={progress_bar}
+                    >
                         <div className='background'>
                             <VideoProgressBar style={{ width: `${progressPercent}%` }} />
-                            <VideoProgressFollower className='follower' style={{ marginLeft: `-${progressPercent}%`, width: `${progressFollowerPercent}%` }} ref={progress_follower} />
+                            <VideoProgressFollower
+                                className='follower' style={{
+                                    marginLeft: `-${progressPercent}%`,
+                                    width: `${progressFollowerPercent}%`
+                                }}
+                                ref={progress_follower}
+                            />
                         </div>
                     </VideoProgress>
                     <VideoVolume>
-                        <div className="volume" ref={volume} onClick={(e) => { updateVolume(e) }}>
+                        <div className="volume" ref={volume} onClick={updateVolume}>
                             <div className="volume_percent" ref={volume_percent} />
                         </div>
                         <VideoButton>
