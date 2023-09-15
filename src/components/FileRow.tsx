760e6df66da7ca94280798c42984161e9d3805c3
mundotv789123 <789michaelfernandes789@gmail.com>
2023-09-15 08:20:03 -0300

    Melhorando nome do arquivo em lista

--- src/components/FileRow.tsx
+++ src/components/FileRow.tsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 const Row = styled.a `
     padding: 12px;
     font-weight: 600;
+    white-space: nowrap;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
     &:hover {
         background: rgba(255, 255, 255, 0.3);
     }
@@ -13,7 +16,7 @@ export default function FileRow(props: {src: string}) {
     const fileName = srcSplited[srcSplited.length - 1];
     return (
         <Row href={props.src}>
-            {fileName.substr(0, 16)+(fileName.length > 16 ? '...' : '')}
+            {fileName}
         </Row>
     )
 }
