f6d86afe8d3e54a4f5c102b2b9e4d89c6576ba62
mundotv789123 <789michaelfernandes789@gmail.com>
2023-09-23 20:05:01 -0300

    Layout player de áudio

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,4 +1,6 @@
-import { AudioContent, AudioElement } from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AudioContent, AudioDurationContent, AudioDurationCount, AudioElement, AudioProgress, AudioProgressBar, AudioTitle, ControlButton, ControlContent, VolumeControl, VolumeProgress, VolumeProgressBar } from "./styles";
+import { faBackwardStep, faForward, faForwardStep, faPause, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function AudioPlayer(props: { src: string | undefined }) {
 
@@ -15,7 +17,29 @@ export default function AudioPlayer(props: { src: string | undefined }) {
     return (
         <AudioContent>
             <AudioElement>
-                <audio controls={true} autoPlay={true} src={props.src} style={{margin: 'auto', width: '80%'}}/>
+                <ControlContent>
+                    <ControlButton><FontAwesomeIcon icon={faBackwardStep} /></ControlButton>
+                    <ControlButton><FontAwesomeIcon icon={faPause} /></ControlButton>
+                    <ControlButton><FontAwesomeIcon icon={faForwardStep} /></ControlButton>
+                    <VolumeControl>
+                        <ControlButton style={{display: 'flex'}}>
+                            <FontAwesomeIcon icon={faVolumeUp} style={{fontSize: '16pt'}}/>
+                        </ControlButton>
+                        <VolumeProgress>
+                            <VolumeProgressBar style={{width: '50%'}}/>
+                        </VolumeProgress>
+                    </VolumeControl>
+                    <AudioTitle>
+                        {fileName}
+                    </AudioTitle>
+                </ControlContent>
+                <AudioDurationContent>
+                    <AudioDurationCount>00:00/00:35</AudioDurationCount>
+                    <AudioProgress>
+                        <AudioProgressBar style={{width: '25%'}}/>
+                    </AudioProgress>
+                </AudioDurationContent>
+                <audio autoPlay={true} src={props.src} style={{margin: 'auto', width: '80%'}}/>
             </AudioElement>
         </AudioContent>
     );
--- src/components/AudioPlayer/styles.ts
+++ src/components/AudioPlayer/styles.ts
@@ -25,11 +25,104 @@ export const AudioContent = styled.div`
 `
 
 export const AudioElement = styled.div`
-    display: flex;
     background: linear-gradient(90deg, #006DAC, #00A2FF);
     box-shadow: 0 4px 4px 0px rgba(0, 0, 0, 0.25);
+    display: flex;
+    flex-direction: column;
     height: 90px;
     border-radius: 15px;
     margin: 10px;
     width: 100%;
+`
+
+export const ControlContent = styled.div`
+    display: flex;
+    margin: 15px 0 0 15px;
+`
+
+export const ControlButton = styled.button`
+    border: none;
+    color: white;
+    background-color: transparent;
+    margin: auto 6px;
+    font-size: 22pt;
+`
+
+export const VolumeControl = styled.div`
+    display: flex;
+`
+
+export const VolumeProgress = styled.div`
+    background-color: #D9D9D9;
+    height: 5px;
+    width: 75px;
+    margin: auto 0;
+    border-radius: 5px;
+`
+
+export const VolumeProgressBar = styled.div`
+    background-color: white;;
+    height: 100%;
+    display: flex;
+    border-radius: 5px;
+    &::after {
+        content: "";
+        display: block;
+        position: relative;
+        margin: -5px -5px 0 auto;
+        width: 5px;
+        height: 5px;
+        background-color: white;
+        border: solid 5px white;
+        border-radius: 50%;
+        box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+    }
+`
+
+export const AudioTitle = styled.h3`
+    white-space: nowrap;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+    text-align: center;
+    width: 100%;
+    margin: auto 15px;
+`
+
+export const AudioDurationContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const AudioDurationCount = styled.p`
+    color: white;
+    font-size: 10pt;
+    margin: 0 15px;
+    text-align: right; 
+`
+
+export const AudioProgress = styled.div `
+    display: flex;
+    background-color: #D9D9D9;
+    height: 5px;
+    margin: auto 15px;
+    border-radius: 5px;
+`
+
+export const AudioProgressBar = styled.div `
+    background-color: white;;
+    height: 100%;
+    display: flex;
+    border-radius: 5px;
+    &::after {
+        content: "";
+        display: block;
+        position: relative;
+        margin: -5px -5px 0 auto;
+        width: 5px;
+        height: 5px;
+        background-color: white;
+        border: solid 5px white;
+        border-radius: 50%;
+        box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+    }
 `
