b53c6826952f84b75a5a502093199b10a748736a
mundotv789123 <789michaelfernandes789@gmail.com>
2023-09-24 16:53:14 -0300

    Adicionando eventos no progress

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,14 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AudioContent, AudioDurationContent, AudioDurationCount, AudioElement, AudioProgress, AudioProgressBar, AudioTitle, ControlButton, ControlContent, VolumeControl, VolumeProgress, VolumeProgressBar } from "./styles";
 import { faBackwardStep, faForwardStep, faPause, faPlay, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
-import { useRef, useState } from "react";
+import { SyntheticEvent, useRef, useState } from "react";
+import { setConstantValue } from "typescript";
 
 export default function AudioPlayer(props: { src: string | undefined }) {
 
     const [playing, setPlaying] = useState(false);
     const [progressPercent, setProgressPercent] = useState(0);
+    const [audioVolume, setAudioVolume] = useState(0);
+
 
     const audio_element = useRef<HTMLAudioElement>();
+    const audio_progress = useRef<HTMLDivElement>();
+    const audio_volume = useRef<HTMLDivElement>();
 
     function updatePlaying() {
         setPlaying(!audio_element.current.paused);
@@ -25,6 +30,21 @@ export default function AudioPlayer(props: { src: string | undefined }) {
     function updateAudioProgress() {
         let percent = (audio_element.current.currentTime * 100 / audio_element.current.duration);
         setProgressPercent(percent);
+    }
+
+    function updateAudioTime(event: any) {
+        let rect = audio_progress.current.getBoundingClientRect();
+        let percent = ((event.clientX - rect.left) * 100 / (rect.right - rect.left));
+        audio_element.current.currentTime = (audio_element.current.duration / 100 * percent);
+        setProgressPercent(percent);
+    }
+
+    function updateAudioVolume(event: any) {
+        let rect = audio_volume.current.getBoundingClientRect();
+        let percent = ((event.clientX - rect.left) * 100 / (rect.right - rect.left));
+        audio_element.current.volume = percent / 100;
+        console.log(audio_element.current.volume)
+        setAudioVolume(percent);
     }
 
     if (props.src == null) {
@@ -50,8 +70,8 @@ export default function AudioPlayer(props: { src: string | undefined }) {
                         <ControlButton style={{display: 'flex'}}>
                             <FontAwesomeIcon icon={faVolumeUp} style={{fontSize: '16pt'}}/>
                         </ControlButton>
-                        <VolumeProgress>
-                            <VolumeProgressBar style={{width: '50%'}}/>
+                        <VolumeProgress onClick={updateAudioVolume} ref={audio_volume}>
+                            <VolumeProgressBar style={{width: `${audioVolume}%`}}/>
                         </VolumeProgress>
                     </VolumeControl>
                     <AudioTitle>
@@ -60,7 +80,7 @@ export default function AudioPlayer(props: { src: string | undefined }) {
                 </ControlContent>
                 <AudioDurationContent>
                     <AudioDurationCount>00:00/00:35</AudioDurationCount>
-                    <AudioProgress>
+                    <AudioProgress onClick={updateAudioTime} ref={audio_progress}>
                         <AudioProgressBar style={{width: `${progressPercent}%`}}/>
                     </AudioProgress>
                 </AudioDurationContent>
