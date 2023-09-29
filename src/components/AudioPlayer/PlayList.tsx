1bba2a7098ec24ccbe28e8f70f8689b1d9dbef2c
mundotv789123 <789michaelfernandes789@gmail.com>
2023-09-29 08:43:24 -0300

    preparando abertura playlist

--- src/components/AudioPlayer/PlayList.tsx
+++ src/components/AudioPlayer/PlayList.tsx
@@ -1,3 +1,29 @@
-export default function PlayList(props: {open: boolean}) {
-    return <>{props.open && <h1>Playlist aqui!</h1>}</>;
+import styled from "styled-components";
+
+const PlayListContent = styled.div`
+    background-color: rgba(0, 0, 0, 0.4);
+    height: 150px;
+    margin-bottom: -10px;
+    padding-bottom: 15px;
+    border-radius: 15px 15px 0 0;
+    overflow: hidden;
+    transition: 500ms;
+`
+
+const PlaylistHeader = styled.div`
+    padding: 10px;
+    text-align: center;
+    border-radius: 15px 15px 0 0;
+    background-color: rgba(0, 0, 0, 0.2);
+`
+
+export default function PlayList(props: { open: boolean }) {
+    return (
+        <PlayListContent style={!props.open ? {height: 0, padding: 0} : {}}>
+            <PlaylistHeader>
+                <h3>Lista de músicas</h3>
+            </PlaylistHeader>
+            <h1>Playlist aqui!</h1>
+        </PlayListContent>
+    );
 }
