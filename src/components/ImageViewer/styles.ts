ad2eee56966a5592850ab05b5913c590a2cb3a5a
mundotv789123 <789michaelfernandes789@gmail.com>
2023-10-25 18:02:16 -0300

    incluindo loading de imagem

--- src/components/ImageViewer/styles.ts
+++ src/components/ImageViewer/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const ImagemContainer = styled.div`
     position: fixed;
@@ -11,8 +11,24 @@ export const ImagemContainer = styled.div`
     background-color: rgba(0, 0, 0, 0.5);
 `
 
+const ImgLodingAnimation = keyframes`
+    0% {
+        background-color: rgb(120, 120, 120);
+    }
+    25% {
+        background-color: rgb(80, 80, 80);
+    }
+    50% {
+        background-color: rgb(80, 80, 80);
+    }
+    75% {
+        background-color: rgb(120, 120, 120);
+    }
+`
+
 export const Img = styled.img`
     max-width: 100%;
     max-height: 100vh;
-    background-color: gray;
+    background-color: rgb(120, 120, 120);
+    animation: ${ImgLodingAnimation} 2s linear infinite;
 `
