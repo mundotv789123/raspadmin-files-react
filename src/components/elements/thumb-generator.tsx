8f0d73b761316ae930d9e8d82d47f9e113b3d786
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-05-20 08:11:16 -0300

    fix: posição do elemento thumb

--- src/components/elements/thumb-generator.tsx
+++ src/components/elements/thumb-generator.tsx
@@ -2,7 +2,7 @@ import { RefObject } from "react";
 
 export function ThumbGenerator(props: {ref?: RefObject<HTMLDivElement | null>}) {
   return (
-    <div className="absolute bg-black bg-opacity-50 p-3 hidden backdrop-blur-sm shadow-sm border-gray-400 border" ref={props.ref}>
+    <div className="fixed bottom-24 bg-black bg-opacity-50 p-3 hidden backdrop-blur-sm shadow-sm border-gray-400 border" ref={props.ref}>
      <div className="bg-gray-50 w-32 h-16">
      </div>
     </div>
