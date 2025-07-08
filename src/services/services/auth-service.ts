9fedb8922973162cc6a4622c1fe4ee85022d793b
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-07-08 08:04:39 -0300

    refresh token periódico

--- src/services/services/auth-service.ts
+++ src/services/services/auth-service.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import ApiBaseService from "../api-base-service";
+import ApiBaseService, { ApiError } from "../api-base-service";
 import { LoginRequest, LoginResponse, RefreshTokenRequest } from "../models/auth-models";
 
 export class AuthService extends ApiBaseService {
@@ -9,7 +9,7 @@ export class AuthService extends ApiBaseService {
     super();
     if (!AuthService.timeoutRefreshToken) {
       if (typeof window !== 'undefined' && window.localStorage) {
-        this.timeoutToRefreshToken(0.5)
+        this.timeoutToRefreshToken()
       }
     }
   }
@@ -19,35 +19,56 @@ export class AuthService extends ApiBaseService {
     localStorage.setItem("token", response.refreshToken)
 
     const tokenData = this.decodeJwtToken(response.token);
-    const currentTime = Math.round(Date.now() / 1000);
-    this.timeoutToRefreshToken(tokenData.exp - currentTime);
+    localStorage.setItem('token_exp', tokenData!.exp.toString());
+
     return response;
   }
 
   async refresh(request: RefreshTokenRequest) {
-    const response = await this.post<LoginResponse>('/auth/login', { ...request, loginType: 'REFRESH_TOKEN' });
-    localStorage.setItem("token", response.refreshToken)
+    try {
+      const response = await this.post<LoginResponse>('/auth/login', { ...request, loginType: 'REFRESH_TOKEN' });
+      localStorage.setItem("token", response.refreshToken)
 
-    const tokenData = this.decodeJwtToken(response.token);
-    const currentTime = Math.round(Date.now() / 1000);
-    this.timeoutToRefreshToken(tokenData.exp - currentTime);
-    return response;
+      const tokenData = this.decodeJwtToken(response.token);
+      localStorage.setItem('token_exp', tokenData!.exp.toString());
+
+      return response;
+    } catch (error) {
+      if (error instanceof ApiError) {
+        if (error.status === 401) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('token_exp');
+        }
+      }
+      throw error;
+    }
   }
 
-  private timeoutToRefreshToken(time: number) {
+  private timeoutToRefreshToken() {
+    const tokenExp = Number(localStorage.getItem('token_exp') ?? '0');
+    const currentTime = Math.round(Date.now() / 1000);
+
+    let time = tokenExp - currentTime;
     time = time < 0 ? 0 : time * .9
+
     if (AuthService.timeoutRefreshToken) {
       clearTimeout(AuthService.timeoutRefreshToken);
     }
     AuthService.timeoutRefreshToken = setTimeout(() => {
       const refreshToken = localStorage.getItem('token');
       if (refreshToken) {
-        this.refresh({ token: refreshToken });
+        this.refresh({ token: refreshToken })
+          .then(() => this.timeoutToRefreshToken())
+          .catch(() => {
+            setTimeout(() => {
+              this.timeoutToRefreshToken();
+            }, 5000);
+          });
       }
     }, time * 1000);
   }
 
-  private decodeJwtToken(jwtStr: string) {
+  private decodeJwtToken(jwtStr: string): { exp: number } | null {
     const base64Splited = jwtStr.split(".");
     if (base64Splited.length === 3) {
       return JSON.parse(atob(base64Splited[1]))
